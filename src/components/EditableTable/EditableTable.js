ddbfc2b372f1a9ef97fa223306153e53b985752c
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-06-26 22:17:09 +0600

    simplified-spreadsheet dynamic column add handler added

--- src/components/EditableTable/EditableTable.js
+++ src/components/EditableTable/EditableTable.js
@@ -3,16 +3,24 @@ import "./EditableTable.css";
 
 const EditableTable = () => {
     const [spreadsheetData, setSpreadsheetData] = useState([
-        { col1: "", col2: "", col3: "", col4: "", col5: "" },
-        { col1: "", col2: "", col3: "", col4: "", col5: "" },
-        { col1: "", col2: "", col3: "", col4: "", col5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
+        { col_1: "", col_2: "", col_3: "", col_4: "", col_5: "" },
     ]);
+    const [headings, setHeadings] = useState(["A", "B", "C", "D", "E"]);
     console.log(spreadsheetData);
 
     //Handle table data change
-    const handleColChange = (event, index, key) => {
+    const handleColChange = (event, index, col) => {
         const list = [...spreadsheetData];
-        list[index][key] = event.target.value;
+        list[index][col] = event.target.value;
         setSpreadsheetData(list);
     };
 
@@ -28,64 +36,50 @@ const EditableTable = () => {
         setSpreadsheetData(list);
     };
 
+    //Handle add Colum
+    const handleAddNewColumn = () => {
+        const list = [...spreadsheetData];
+        const keys = Object.keys(list[0]);
+        const lastItem = +keys[keys.length - 1].split("_")[1];
+        list.forEach((row) => (row[`col_${lastItem + 1}`] = ""));
+        setSpreadsheetData(list);
+
+        //updating heading
+        let char = headings[headings.length - 1];
+        char = String.fromCharCode(char.charCodeAt(0) + 1);
+        setHeadings([...headings, char]);
+    };
+
     return (
         <section id="simplified-spreadsheet">
-            <button onClick={handleAddNewRow}>Add New Row</button>
+            <div className="actions">
+                <button onClick={handleAddNewRow}>Add New Row</button>
+                <button onClick={handleAddNewColumn}>Add New column</button>
+                <button onClick={handleAddNewColumn}>Download CSV</button>
+            </div>
             <table className="editable-table">
                 <thead>
                     <tr>
                         <th></th>
-                        <th>A</th>
-                        <th>B</th>
-                        <th>C</th>
-                        <th>D</th>
-                        <th>E</th>
+                        {headings.map((heading, hk) => (
+                            <th key={hk}>{heading}</th>
+                        ))}
                     </tr>
                 </thead>
                 <tbody>
                     {spreadsheetData.map((tr, index) => (
                         <tr key={index}>
                             <th>{index + 1}</th>
-                            <td>
-                                <input
-                                    onChange={(event) =>
-                                        handleColChange(event, index, "col1")
-                                    }
-                                    type="text"
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    onChange={(event) =>
-                                        handleColChange(event, index, "col2")
-                                    }
-                                    type="text"
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    onChange={(event) =>
-                                        handleColChange(event, index, "col3")
-                                    }
-                                    type="text"
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    onChange={(event) =>
-                                        handleColChange(event, index, "col4")
-                                    }
-                                    type="text"
-                                />
-                            </td>
-                            <td>
-                                <input
-                                    onChange={(event) =>
-                                        handleColChange(event, index, "col5")
-                                    }
-                                    type="text"
-                                />
-                            </td>
+                            {Object.keys(tr).map((col) => (
+                                <td key={col}>
+                                    <input
+                                        onChange={(event) =>
+                                            handleColChange(event, index, col)
+                                        }
+                                        type="text"
+                                    />
+                                </td>
+                            ))}
                         </tr>
                     ))}
                 </tbody>
