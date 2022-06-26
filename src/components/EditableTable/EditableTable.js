697155c97ed63cbd41892129f0687f1c796456ad
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-06-26 19:25:35 +0600

    simplified-spreadsheet editable row and add new row handler added

--- src/components/EditableTable/EditableTable.js
+++ src/components/EditableTable/EditableTable.js
@@ -1,9 +1,36 @@
-import React from "react";
-import "./EditableTable.css"
+import React, { useState } from "react";
+import "./EditableTable.css";
 
 const EditableTable = () => {
+    const [spreadsheetData, setSpreadsheetData] = useState([
+        { col1: "", col2: "", col3: "", col4: "", col5: "" },
+        { col1: "", col2: "", col3: "", col4: "", col5: "" },
+        { col1: "", col2: "", col3: "", col4: "", col5: "" },
+    ]);
+    console.log(spreadsheetData);
+
+    //Handle table data change
+    const handleColChange = (event, index, key) => {
+        const list = [...spreadsheetData];
+        list[index][key] = event.target.value;
+        setSpreadsheetData(list);
+    };
+
+    //Handle add Row
+    const handleAddNewRow = () => {
+        const list = [...spreadsheetData];
+        const columns = Object.keys(list[0]);
+        const newRow = {};
+        columns.forEach((column) => {
+            newRow[column] = "";
+        });
+        list.push(newRow);
+        setSpreadsheetData(list);
+    };
+
     return (
         <section id="simplified-spreadsheet">
+            <button onClick={handleAddNewRow}>Add New Row</button>
             <table className="editable-table">
                 <thead>
                     <tr>
@@ -16,30 +43,51 @@ const EditableTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th>1</th>
-                        <td>Book Name</td>
-                        <td>Author</td>
-                        <td>Price</td>
-                        <td>Quantity</td>
-                        <td>Category</td>
-                    </tr>
-                    <tr>
-                        <th>2</th>
-                        <td>Bangla</td>
-                        <td>Eity</td>
-                        <td>500</td>
-                        <td>5</td>
-                        <td>Board book</td>
-                    </tr>
-                    <tr>
-                        <th>3</th>
-                        <td>English</td>
-                        <td>Touhid</td>
-                        <td>200</td>
-                        <td>2</td>
-                        <td>Board Book</td>
-                    </tr>
+                    {spreadsheetData.map((tr, index) => (
+                        <tr key={index}>
+                            <th>{index + 1}</th>
+                            <td>
+                                <input
+                                    onChange={(event) =>
+                                        handleColChange(event, index, "col1")
+                                    }
+                                    type="text"
+                                />
+                            </td>
+                            <td>
+                                <input
+                                    onChange={(event) =>
+                                        handleColChange(event, index, "col2")
+                                    }
+                                    type="text"
+                                />
+                            </td>
+                            <td>
+                                <input
+                                    onChange={(event) =>
+                                        handleColChange(event, index, "col3")
+                                    }
+                                    type="text"
+                                />
+                            </td>
+                            <td>
+                                <input
+                                    onChange={(event) =>
+                                        handleColChange(event, index, "col4")
+                                    }
+                                    type="text"
+                                />
+                            </td>
+                            <td>
+                                <input
+                                    onChange={(event) =>
+                                        handleColChange(event, index, "col5")
+                                    }
+                                    type="text"
+                                />
+                            </td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </section>
